502c11bb2da5e5096c75a3d1ccacf01fb8b39d07
Sylvain (Snipy) Julmy <sylvain.julmy@gmail.com>
2022-12-03 20:07:43 +0100

    [aoc] day03 scala3

---
+++ scala3/day03/src/main/scala/Main.scala
@@ -0,0 +1,40 @@
+import scala.util.Using
+
+case class Rucksack(left: String, right: String)
+
+object Rucksack:
+  def fromString(string: String): Rucksack =
+    val (left, right) = string.splitAt(string.length() / 2)
+    Rucksack(left, right)
+
+def priority(item: Char): Int = item match {
+  case c if c.toInt >= 'a'.toInt && c.toInt <= 'z'.toInt =>
+    c.toInt - 'a'.toInt + 1
+  case c if c.toInt >= 'A'.toInt && c.toInt <= 'Z'.toInt =>
+    c.toInt - 'A'.toInt + 27
+  case _ =>
+    sys.exit(1)
+}
+
+@main def main(filepath: String): Unit =
+  val input = Using(scala.io.Source.fromFile(filepath))(source =>
+    source.getLines().toList
+  ).getOrElse { println("Unable to read $filepath"); sys.exit(1) }
+
+  val score1: Int = input.map { line =>
+    val (left, right) = line.splitAt(line.length() / 2)
+    val duplicate = left.toList.intersect(right.toList).head
+    priority(duplicate)
+  }.sum
+
+  val score2: Int = input
+    .grouped(3)
+    .map { case List(a, b, c) =>
+      val badge = a.toList.intersect(b.toList.intersect(c.toList)).head
+      priority(badge)
+    }
+    .sum
+
+  println(s"Score part 1 : $score1")
+
+  println(s"Score part 2 : $score2")
